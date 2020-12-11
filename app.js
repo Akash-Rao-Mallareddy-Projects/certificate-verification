199380434bf41c725520c658eab93b57d5192e99
Abhinav Chandu Chenumalla <abhinavchandu242@gmail.com>
2020-12-11 19:01:41 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,117 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+var path = require('path');
+var sslRedirect = require('heroku-ssl-redirect');
+var robots = require('robots.txt');
+var compression = require('compression');
+
+
+const app = express();
+app.use(sslRedirect());
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(compression());
+app.use(express.static("public"));
+app.use(require('prerender-node'));
+app.use( require('express-force-domain')('https://www.girlscripthyderabad.tech'));
+app.use(robots(__dirname + '/robots.txt'));
+
+
+mongoose.connect("mongodb+srv://girlscripthyderabad:hyd@girlscript@cluster0.p3ud3.mongodb.net/certificateDB", {useNewUrlParser: true, useUnifiedTopology: true});
+const certificateSchema = {
+  certNo : String,
+  event : String,
+  date : String,
+  mail : String,
+  name : String,
+  role : String,
+  certUrl : String,
+  description : String
+};
+
+const Certificate = mongoose.model("Certificate",certificateSchema);
+
+
+
+
+//rooting
+app.get("/", function(req,res){
+  res.render("index");
+});
+app.get("/events", function(req,res){
+  res.render("events");
+});
+app.get("/halloffame", function(req,res){
+  res.render("halloffame");
+});
+app.get("/links", function(req,res){
+  res.render("links");
+});
+app.get("/team", function(req,res){
+  res.render("team");
+});
+app.get("/verify-long", function(req,res){
+  res.render("verify-long");
+});
+
+app.get('/sitemap.xml', function(req, res){
+    res.contentType('application/xml');
+    res.sendFile(path.join(__dirname , 'sitemap.xml'));
+});
+
+
+//verifyurl
+app.get("/verify", function(req, res){
+  const requestedCertId = req.query.certno;
+  console.log(requestedCertId);
+
+  Certificate.findOne({certId:requestedCertId},function(err,certificate){
+    if(!err){
+      if(!certificate){
+        res.render("failure");
+      }
+      else{
+        res.render("verify",{
+          certid:requestedCertId,
+          certNo : certificate.certId,
+          event : certificate.event,
+          date : certificate.date,
+          mail : certificate.mail,
+          name:certificate.name,
+          role:certificate.role,
+          certUrl: certificate.certUrl,
+          description: certificate.description
+        });
+      }
+  }
+
+  });
+
+});
+
+app.post("/verify", function(req, res){
+  const certId = req.body.certid;
+  res.redirect("https://www.girlscripthyderabad.tech/verify/?certno=" +certId);
+});
+
+
+app.use(function(req,res){
+    res.status(404).render("404");
+});
+
+
+
+//end
+
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+app.listen(port,function(){
+  console.log("Server is running successfully.")
+});
