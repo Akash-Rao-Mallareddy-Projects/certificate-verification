8d66fbad6f7e96642520f361ccfc905c383882b0
Akash Mallareddy <47551241+Akashmallareddy@users.noreply.github.com>
2020-12-11 21:42:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(compression());
 app.use(express.static("public"));
 app.use(require('prerender-node'));
-app.use( require('express-force-domain')('https://akash-rao-mallareddy-projects.github.io/certificate-verification/'));
+## app.use( require('express-force-domain')('https://akash-rao-mallareddy-projects.github.io/certificate-verification/'));
 app.use(robots(__dirname + '/robots.txt'));
 
 
